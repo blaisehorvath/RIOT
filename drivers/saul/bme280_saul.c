d06e8d328d48c34ec74110a1a4fd7c79098f3571
blaisehorvath <blaisehorvath@gmail.com>
2016-07-22 23:12:31 +0200

    Driver is working

--- drivers/saul/bme280_saul.c
+++ drivers/saul/bme280_saul.c
@@ -24,18 +24,36 @@
 #include "phydat.h"
 #include "periph/gpio.h"
 
+#include "bme280.h"
+
+#define ENABLE_DEBUG (1)
+#include "debug.h"
+
 
 static int read(void *dev, phydat_t *res)
 {
+    /*BOSCH code*/
+
+	s32 results = bme280_data_readout_template();
+
+
+	/*RIOT code*/
+	res->val[0] = 0;
+    memset(&(res->val[1]), 0, 2 * sizeof(int16_t));
+    res->unit = UNIT_BOOL;
+    res->scale = 0;
+    DEBUG("READ FROM BME280\n");
     return 1;
 }
 
 static int write(void *dev, phydat_t *state)
 {
+	state->val[0] = 1;
     return 1;
+    DEBUG("WRITE TO BME280\n");
 }
 
-const saul_driver_t gpio_saul_driver = {
+const saul_driver_t i2c_saul_driver = {
     .read = read,
     .write = write,
     .type = SAUL_ACT_SWITCH,
--- sys/auto_init/saul/auto_init_saul_bme280.c
+++ sys/auto_init/saul/auto_init_saul_bme280.c
@@ -3,12 +3,72 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#include "periph_conf.h"
+#include "periph/i2c.h"
+
+#include "saul_reg.h"
+#include "saul/periph.h"
+
+#define BUFSIZE        (128U)
+
+/**
+ * @brief: Hard coded device variables
+ */
+#define i2c_dev 0
+#define i2c_speed 1
+#define addr 0x77 //uint 119
+#define reg 0xd0 //uint 208
+#define length 1 /* read 1 byte*/
 
 #define ENABLE_DEBUG (1)
 #include "debug.h"
 
+extern saul_driver_t i2c_saul_driver;
+
+static i2c_t bme_280_dev_id = 0; /* i2c device id, equivalent to the value of the i2c_dev variable */
+
+static saul_reg_t bme280 = {
+	.next = 0,
+	.dev = &bme_280_dev_id,
+	.driver = &i2c_saul_driver,
+	.name = "BME280"
+};
+
 void auto_init_bme280(void){
-	DEBUG("budos fasz\n");
+
+	/* Initializing the device */
+    DEBUG("auto init bme280 SAUL\n");
+
+    int res = i2c_init_master(i2c_dev, i2c_speed);
+
+    if (res == -1) {
+        puts("Error: Init: Given device not available");
+    }
+    else if (res == -2) {
+        puts("Error: Init: Unsupported speed value");
+    }
+    else {
+
+    	/* Checking the device id if it's successfully initialized */
+
+        printf("I2C_%i successfully initialized as master!\n", i2c_dev);
+        char data[BUFSIZE];
+        res = i2c_read_regs(i2c_dev, (uint8_t)addr, (uint8_t)reg, data, length);
+
+        int n = sizeof(data) / sizeof(int);
+        unsigned int device_id = (unsigned int)data[0];
+
+        if (n == 32 && device_id == (unsigned int)96) {
+    		DEBUG("The ID of the BME280 device is: 0x%02x \n", device_id);
+
+    		/* adding the device to the SAUL*/
+
+    		saul_reg_add(&(bme280));
+
+        } else {
+        	printf("ERROR: BME280 device cannot be accessed!\n");
+        }
+    }
 }
 
 #endif
